add6dc289991edea1c2f33c787513b9bc42e6edb
alonbh6 <49645142+alonbh6@users.noreply.github.com>
2019-12-20 16:06:02 +0200

    Finished

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -5,7 +5,7 @@
 /*Maze Creator and Solver
 by Alon Ben Harosh and Shira ostrinsky
 Choose 1 to enter your own maze, or 2 to create a random maze.
-Then enter wanted higth (3-20) and width (3-80) of the maze: need to be odd number for random maze!
+Then enter wanted higth and width (Max is 99x99) of the maze: need to be odd number for random maze!
 If you chose 1: enter the string of chars with only: '*' for wall, or 'space' for blank
 The frame must be only '*' and spce for opening point at (1,0) and exit point at (hight-2,width-1)
 ENJOY!!!!n */
@@ -14,7 +14,7 @@ int main()
 {
 	int choice, h, w;
 
-	cout << "Enter an Hight and Widht (uneven number - max 99) for a random maze:" << endl;
+	cout << "Enter an Hight and Widht (odd number - max 99) for a random maze:" << endl;
 
 	
 	cin >> h>> w;
